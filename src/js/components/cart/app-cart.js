d9c4c4d89341672ee5d1f91f1f1f6d698971999c
raphaelkyrdan <rafaelkyrdan@gmail.com>
2015-11-30 18:07:16 +0200

    Cleaned codebase from commented code

--- src/js/components/cart/app-cart.js
+++ src/js/components/cart/app-cart.js
@@ -8,66 +8,43 @@ const cartItems = () => {
 	return { items: AppStore.getCart() };
 }
 
-//class Cart extends React.Component {
 const Cart = (props) => {
 
-	// constructor(){
-	// 	super();
-	// 	this.state = cartItems();
-	// 	this._onChange = this._onChange.bind(this);
-	// }
+	var total = 0;
+	var items = props.items.map((item, i) => {	
+		let subtotal = item.cost * item.qty;
+		total += subtotal;
 
-	// componentWillMount(){
-	// 	AppStore.addChangeListener( this._onChange );
-	// }
+		return (<AppCartItem
+					subtotal={subtotal}
+					key={i}
+					item={item} />);
+	});
 
-	// componentWillUnmount(){
-	// 	AppStore.removeChangeListener( this._onChange );
-	// }
-
-	// _onChange(){
-	// 	this.setState( cartItems )
-	// }
-
-	//render(){
-
-		var total = 0;
-		//var items = this.state.items.map((item, i) => {
-		var items = props.items.map((item, i) => {	
-			let subtotal = item.cost * item.qty;
-			total += subtotal;
-
-			return (<AppCartItem
-						subtotal={subtotal}
-						key={i}
-						item={item} />);
-		});
-
-		return (<div>
-					<h1>Cart</h1>
-					<table className="table table-hover">
-				      <thead>
-				        <tr>
-				          <th>#</th>
-				          <th>Item</th>
-				          <th>Quantaty</th>
-				          <th>Actions</th>
-				          <th>Total</th>
-				        </tr>
-				      </thead>
-				      <tbody>
-				        {items}
-				      </tbody>
-				      <tfoot>
-				      	<tr>
-				      		<td colSpan="4" className="text-right">Total</td>
-				      		<td>$ {total}</td>
-				      	</tr>
-				      </tfoot>
-				    </table>
-				    <Link to="/">Continue shopping</Link>					
-				</div>);
-	//}
+	return (<div>
+				<h1>Cart</h1>
+				<table className="table table-hover">
+			      <thead>
+			        <tr>
+			          <th>#</th>
+			          <th>Item</th>
+			          <th>Quantaty</th>
+			          <th>Actions</th>
+			          <th>Total</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			        {items}
+			      </tbody>
+			      <tfoot>
+			      	<tr>
+			      		<td colSpan="4" className="text-right">Total</td>
+			      		<td>$ {total}</td>
+			      	</tr>
+			      </tfoot>
+			    </table>
+			    <Link to="/">Continue shopping</Link>					
+			</div>);
 }
 
 export default StoreWatchMixin(Cart, cartItems);
